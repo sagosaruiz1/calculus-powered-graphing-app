3c4773654c81b2276097793aabd72a8f3949132f
Ruiz Sagosa <ruizsagosa05>
2026-04-25 15:22:25 +0800

    adding menu

--- calc_logic.py
+++ calc_logic.py
@@ -7,7 +7,8 @@ def get_derivative(f, x_vals, dx=1e-6):
 
     return (f(x_vals + dx) - f(x_vals)) / dx
 
-def get_integral(f, start, end):
-
-    area, error = quad(f, start, end)
+def get_integral(f1, f2, start, end):
+  
+    diff_func = lambda x: abs(f1(x) - f2(x))
+    area, error = quad(diff_func, start, end)
     return area
--- visualizer.py
+++ visualizer.py
@@ -1,22 +1,27 @@
 import matplotlib.pyplot as plt
 
 
-def generate_plot(x_values, y_values, deriv_values, func_str):
-
+def generate_plot(x_values, y1_values, y2_values, deriv_values, f1_str, f2_str, mode):
     plt.figure(figsize=(10,6));
 
-    plt.plot(x_values, y_values, label=f"Original: f(x) = {func_str}", color='blue', linewidth=2)
+    plt.plot(x_values, y1_values, label=f"f(x) = {f1_str}", color='blue', lw=2)
+    plt.plot(x_values, deriv_values, label=f"f'(x)", color='red', linestyle='--')
 
-    plt.plot(x_values, deriv_values, label=f"Derivative: f'(x)", color='red', linestyle='--')
+    if mode == "Between Curves":
+        plt.plot(x_values, y2_values, label=f"g(x) = {f2_str}", color='purple', lw=2)
+    else:
+        plt.axhline(0, color='black', lw=1)
+        plt.title(f"Area under Curve: {f1_str}")
 
-    plt.fill_between(x_values, y_values, color='orange', alpha=0.3, label="Integral (Area)")
+    plt.fill_between(x_values, y1_values, y2_values, color='orange', alpha=0.3, label="Integral Area")
+
 
     plt.axhline(0, color='black', linewidth=1)
     plt.axvline(0, color='black', linewidth=1)
 
     plt.title(f"Calculus Visualization for {func_str}", fontsize=14)
-    plt.xlabel("x values", fontsize=12)
-    plt.ylabel("y values", fontsize=12)
+    plt.xlabel("x")
+    plt.ylabel("y")
     plt.legend()
     plt.grid(True, linestyle=':', alpha=0.6)
 
